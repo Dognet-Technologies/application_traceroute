f27a257cc3831d3eb39ffa879e5f9534074acb16
simone.lapdog4Lenovo <simonebellometti@dognet.tech>
2026-02-19 17:02:49 +0100

    Aggiunto skip e cap per ridurre il tempo di testing in caso di troppi bypass

--- security-testing-suite/core/crawler/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Smart Vulnerability Crawler module
-"""
-from .smart_vuln_crawler2 import SmartCrawler
-
-# Alias per backward compatibility
-SmartVulnerabilityCrawler = SmartCrawler
-
-__all__ = ['SmartCrawler', 'SmartVulnerabilityCrawler']
--- security-testing-suite/core/traceroute/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Application Stack Traceroute module
-"""
-from .application_traceroute_v3_5 import ProgressiveStackAnalyzer
-
-__all__ = ['ProgressiveStackAnalyzer']
